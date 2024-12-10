44772b48fdd9eede9d51682ce8baf6449ed6d3f6
bettyje <bettyjelagat156@gmail.com>
2024-12-10 23:00:07 +0300

    CRUD operations on relational databases using SQL.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+    ('Rowdy', 5, 'M', 'Black', 'Aggressive', 1),
+    ('Tabitha', 7, 'F', 'White', 'Playful', 1),
+    ('Sergeant Brown', 15, 'M', 'Brown', 'Serious', 0),
+    ('Melissa', 8, 'F', 'Golden', 'Friendly', 1),
+    ('Grinch', 12, 'M', 'Green', 'Grumpy', 0),
+    ('Wendy', 4, 'F', 'Black', 'Clever', 1),
+    (NULL, 2, 'M', 'Brown', 'Shy', 1); 
